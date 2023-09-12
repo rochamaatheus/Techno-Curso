c5a27789ac523596f36ab8d8089db1008fe55c6d
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-09-12 18:10:01 -0300

    build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
             <h2>Avaliações</h2>
             <ul>
               <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
-                <p>{{ avaliacao.nome }}</p>
+                <p>- {{ avaliacao.nome }}</p>
                 <p>{{ avaliacao.descricao }}</p>
               </li>
             </ul>
